57700113e8dce91d2087dc1c60d2390a728edec9
Table 1 <t1@anadeainc.com>
2018-04-12 11:10:12 +0300

    Demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "application#new"
+  get "/welcome", to: "welcome#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
