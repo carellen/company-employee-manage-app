9cf4c8cf7dff558300676f908009236df051a818
Table 1 <t1@anadeainc.com>
2018-04-18 13:54:02 +0300

    Add user authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "user#index"
 
   resources :admin, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :dashboard, only: :index
+  end
 end
